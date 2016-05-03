edb112e9697ee907a5a9036b6f18410a1780d59d
eMedin4 <elannmedina@gmail.com>
2016-05-03 12:49:50 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Carbon::setLocale('es');
-        setlocale(LC_TIME,'de_DE.UTF-8');
+        setlocale(LC_TIME, 'German');
     }
 
     /**
